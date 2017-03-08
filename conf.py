b92f9214e94472f8b5b3159cae62ff8ed2dbb503
qp <qp@gravediggerdeMac-mini.local>
2017-03-08 13:59:37 +0800

    modify queue

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-access_key_id = 'LTAIghNrxXw40red'
-access_key_secret = 'G6y2Mw4OFExVyoP4WM7Ic7prnWU3WY'
+access_key_id = 'ROwmpMaoM0G9cd7Z'
+access_key_secret = 'g5ZAjXnl3GERQZ1rhm7ytdxR8obBUH'
 oss_endpoint = 'oss-cn-shanghai.aliyuncs.com'
 oss_img_path = u'http://jpg-test-yzj.oss-cn-shanghai.aliyuncs.com/'
 queue_endpoint = 'http://1418269445122937.mns.cn-shanghai.aliyuncs.com/'
